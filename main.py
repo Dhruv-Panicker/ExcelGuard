7e7d94b9301eb1b3f90a5ad5e330a2dae92137a3
pJeyakumar <jn.piranavan@gmail.com>
2024-01-21 20:24:07 -0500

    feat: displaying scan_list page and applying bootstrap styling

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask 
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route("/")
 def index(): 
-    return "Congratulations, it's a web app!"
+    return render_template("scan_list.html")
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
