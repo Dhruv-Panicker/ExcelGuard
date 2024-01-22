b4fde8290ad92d9f074e6a77c9657efa55b58e72
pJeyakumar <jn.piranavan@gmail.com>
2024-01-22 12:56:41 -0500

    feat: creating multiple endpoints and their necessary template files

--- main.py
+++ main.py
@@ -4,10 +4,27 @@ from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 Bootstrap(app)
-
-@app.route("/")
-def index(): 
-    return render_template("scan_list.html")
+@app.route("/login")
+def login():
+  return render_template("login.html")
+@app.route("/scan_list")
+def scan_list(): 
+  return render_template("scan_list.html")
+@app.route("/scanning")
+def scanning():
+  return render_template("scanning.html")
+@app.route("/scan_results")
+def scan_results():
+  return render_template("scan_results.html")
+@app.route("/view_scan")
+def view_scan():
+  return render_template("view_scan.html")
+@app.route("/settings")
+def settings():
+  return render_template("settings.html")
+@app.route("/logout")
+def logout():
+  return render_template("logout.html")
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+  app.run(host="127.0.0.1", port=8080, debug=True)
