a13dc356b3f8372572faea8615a46a55d24ba824
pJeyakumar <jn.piranavan@gmail.com>
2024-01-27 21:46:20 -0500

    chore: fixing merge conflicts with Login functionality

--- main.py
+++ main.py
@@ -1,23 +1,81 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, render_template, url_for, redirect
 from flask_bootstrap import Bootstrap
-import pandas as pd
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user 
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
 import os
+import pandas as pd
 from openpyxl import load_workbook
 from openpyxl.styles.fonts import Font
 
-
-
 app = Flask(__name__)
 Bootstrap(app)
-@app.route("/login")
-def login():
-  return render_template("login.html")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SECRET_KEY"] = "thisisasecretkey"
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@app.route("/")
+def index(): 
+  return render_template("home.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login(): 
+  form = LoginForm()
+  if form.validate_on_submit():
+    user = User.query.filter_by(username=form.username.data).first()
+    if user: 
+      if bcrypt.check_password_hash(user.password, form.password.data):
+        login_user(user)
+        return redirect(url_for("dashboard"))
+  return render_template("login.html", form=form)
+
+@app.route("/dashboard", methods=["GET", "POST"])
+@login_required
+def dashboard(): 
+  return render_template("dashboard.html")
+
+@app.route("/logout", methods=["GET", "POST"])
+@login_required
+def logout():
+  logout_user()
+  return redirect(url_for("login"))
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+  form = RegistrationForm()
+
+  if form.validate_on_submit():
+    hashed_password = bcrypt.generate_password_hash(form.password.data)
+    new_user = User(username=form.username.data, password=hashed_password)
+
+    db.session.add(new_user)
+    try:
+      db.session.commit()
+      print("User added successfully")  # Debugging message
+    except Exception as e:
+      print("Failed to add user:", e) 
+      return redirect(url_for("login"))
+    else:
+      print("Form not validated")  # Debugging message
+      print(form.errors)         
+    print("Database file path:", os.path.join(os.getcwd(), "database.db"))
+    return render_template("register.html", form=form)
 
 @app.route("/scan_list")
 def scan_list(): 
   return render_template("scan_list.html")
 
-@app.route("/begin_scan", methods=['POST'])
+@app.route("/begin_scan", methods=["POST"])
 def begin_scan():
   template_file = None
   # If TemplateFile is uploaded assign it to our templateFile variable to it
@@ -67,9 +125,30 @@ def view_scan():
 def settings():
   return render_template("settings.html")
 
-@app.route("/logout")
-def logout():
-  return render_template("logout.html")
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin): 
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), nullable=False, unique=True)
+  password = db.Column(db.String(80), nullable=False)
+
+class RegistrationForm(FlaskForm): 
+  username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+  password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+  submit = SubmitField("Register")
+
+  def validate_username(self, username): 
+    existing_user_username = User.query.filter_by(username=username.data).first()
+
+    if existing_user_username: 
+      raise ValidationError("This username already exists. PLease choose another one.")
+
+class LoginForm(FlaskForm): 
+  username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+  password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+  submit = SubmitField("Login")
 
 if __name__ == "__main__":
   app.run(host="127.0.0.1", port=8080, debug=True)
