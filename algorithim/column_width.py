c5ebc947f8ba1cf6ee2291040b33259667c590ca
Dhruv <dpanicke@uwo.ca>
2024-03-28 20:32:58 -0400

    Algorithim:Coloumn_Width Check Implementaion

--- algorithim/column_width.py
+++ algorithim/column_width.py
@@ -1,4 +1,24 @@
-def check_coloumn_data(file_data):
-    # Your code to check the coloumn data
-    # Return a score or a result indicating the likelihood of plagiarism
-    pass
+def check_coloumn_data(file_column_data, template_column_data):
+    #Set where all the suspicious files will be added 
+    suspicious_files = set()
+
+    #Convert the template file coloumn data into a set for lookup 
+    template_column_data = set(template_column_data)
+
+    # Store a dictionary mapping the column width set to the filenames that have it
+    column_set_to_files = {}
+
+    for filename, columns in file_column_data.items(): 
+        if set(columns) == template_column_data: 
+            continue
+        # hashable version of a set that can be used as a key in a dictionary.
+        column_width_key = frozenset(columns)  
+
+        if column_width_key not in column_set_to_files: 
+            column_set_to_files[column_width_key] = {filename}
+        else: 
+            #If the theres already a file with this coloumn width then mark it as suspicious
+            column_set_to_files[column_width_key].add(filename)
+            suspicious_files.update(column_set_to_files[column_width_key])
+    
+    return list(suspicious_files)
--- algorithim/plagiarism_checker.py
+++ algorithim/plagiarism_checker.py
@@ -6,11 +6,34 @@ from .font_data import check_font_data
 from .link_data import check_link_data
 from .formula_data import check_formula_data
 
+# algorithm/plagiarism_checker.py
+#Function that will get all data from the beign_scan function 
+def get_file_data(author_data, column_data, font_data, formula_data, chart_data):
+    file_data = {
+        'author_data': author_data,
+        'column_data': column_data,
+        'font_data': font_data,
+        'formula_data': formula_data,
+        'chart_data': chart_data
+    }
+    return file_data
+
+def get_template_file_data(author_data, column_data, font_data, formula_data, chart_data):
+    template_file_data = {
+        'author_data': author_data,
+        'column_data': column_data,
+        'font_data': font_data,
+        'formula_data': formula_data,
+        'chart_data': chart_data
+    }
+    return template_file_data
+
+
 #THIS IS A PLACEHOLDER FUNCTION THAT WILL JUST CALL AND AGGREGATE ALL THE CHECKS 
-def perform_checks(file_data):
+def perform_checks(file_data, template_file_data):
     # Calculate scores from each individual check
     fingerprint_score = check_fingerprint(file_data)
-    column_width_score = check_column_width(file_data)
+    column_width_files = check_column_width(file_data['column_data'], template_file_data['column_data'])
     author_data_score = check_author_data(file_data)
     shape_data_score = check_shape_data(file_data)
     font_data_score = check_font_data(file_data)
@@ -18,7 +41,7 @@ def perform_checks(file_data):
     formula_data_score = check_formula_data(file_data)
 
     # Aggregate the scores.
-    total_score = (fingerprint_score + column_width_score + author_data_score +
+    total_score = (fingerprint_score + column_width_files + author_data_score +
                    shape_data_score + font_data_score + link_data_score +
                    formula_data_score) / 7  # Example averaging 
 
