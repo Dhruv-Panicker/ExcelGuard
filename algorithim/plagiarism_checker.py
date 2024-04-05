4e30e89a85da94619339e76521711f36065a1648
pJeyakumar <jn.piranavan@gmail.com>
2024-04-05 19:43:55 -0400

    chore: uncomments, double quotes, and calling the necessary databases models in functions that use them

--- algorithim/plagiarism_checker.py
+++ algorithim/plagiarism_checker.py
@@ -10,22 +10,21 @@ DEFAULT_SERIES_FILE_DATA_SOURCE = "Current Worksheet"
 FORMULA_INSIDE_BRACKETS = r'\(([^)]+)\)'
 FORMULA_INSIDE_SQUARE_BRACKETS = r'\[([^\]]+)\]'
 
-#THIS IS A PLACEHOLDER FUNCTION THAT WILL JUST CALL AND AGGREGATE ALL THE CHECKS 
 def perform_checks(scan_id, db, ExcelFile, ExcelChart, TemplateFile):
-  # template_data = get_template_file_data(scan_id)
-  # fingerprint_data = get_fingerprint_data(scan_id)
-  # column_width_data = get_column_width_data(scan_id)
-  # author_data = get_author_data(scan_id)
+  template_data = get_template_file_data(scan_id, TemplateFile)
+  # fingerprint_data = get_fingerprint_data(scan_id, ExcelFile)
+  # column_width_data = get_column_width_data(scan_id, ExcelFile)
+  # author_data = get_author_data(scan_id, ExcelFile)
   font_data = get_font_data(scan_id, db, ExcelFile)
   chart_data = get_chart_data(scan_id, db, ExcelFile, ExcelChart)
-  # formula_data = get_formula_data(scan_id)
+  # formula_data = get_formula_data(scan_id, ExcelFile)
 
   # Calculate scores from each individual check
   chart_data_scores = 0
   # fingerprint_score = check_fingerprint_data(fingerprint_data)
-  # column_width_files = check_column_width(column_width_data, template_data['column_data'] if template_data else [])
-  # author_data_files = check_author_data(author_data, template_data['author_data'] if template_data else [])
-  font_component_score = check_font_data(font_data)
+  # column_width_files = check_column_width(column_width_data, template_data["column_data"] if template_data else [])
+  # author_data_files = check_author_data(author_data, template_data["author_data"] if template_data else [])
+  font_component_score = check_font_data(font_data, template_data)
   chart_component_score = check_chart_data(chart_data)
   # formula_data_score = check_formula_data(formula_data)
 
@@ -37,7 +36,7 @@ def perform_checks(scan_id, db, ExcelFile, ExcelChart, TemplateFile):
   # Return the total score
   return chart_component_score
 
-def get_fingerprint_data(scan_id):
+def get_fingerprint_data(scan_id, ExcelFile):
     files = ExcelFile.query.filter_by(scan_id=scan_id).all()
     fingerprint_data = {}
 
@@ -48,13 +47,13 @@ def get_fingerprint_data(scan_id):
       formula_data = file.complex_formulas_list 
 
       fingerprint_data[file.file_name] = {
-        'author_data': author_data, 
-        'formula_data': formula_data, 
+        "author_data": author_data, 
+        "formula_data": formula_data, 
       }
     return fingerprint_data
 
 
-def get_column_width_data(scan_id, db, ExcelFile):
+def get_column_width_data(scan_id, ExcelFile):
   # Query all excel_files which have the scan_id
   files = ExcelFile.query.filter_by(scan_id=scan_id).all()
   column_width_data = {}
@@ -68,7 +67,7 @@ def get_column_width_data(scan_id, db, ExcelFile):
     column_width_data[file_name] = unique_column_width_list
   return column_width_data
 
-def get_author_data(scan_id, db, ExcelFile):
+def get_author_data(scan_id, ExcelFile):
   files = ExcelFile.query.filter_by(scan_id=scan_id).all()
   author_data = {}
   
@@ -140,22 +139,22 @@ def get_formula_data(scan_id, db, ExcelFile):
     formula_data[file.file_name] = file.complex_formulas_list
   return formula_data
 
-#Function that will get all data from the tempate dile from db 
-def get_template_file_data(scan_id):
+# Function that will get all data from the tempate file from db 
+def get_template_file_data(scan_id, TemplateFile):
     template_file = TemplateFile.query.filter_by(scan_id=scan_id).first()
 
     if template_file: 
       author_data = {
-            "created": template_file.created,
-            "creator": template_file.creator,
+        "created": template_file.created,
+        "creator": template_file.creator,
         }
       column_data = template_file.unique_column_width_list
       font_data = template_file.unique_font_names_list
 
       template_file_data = {
-        'author_data': author_data,
-        'column_data': column_data,
-        'font_data': font_data,
+        "author_data": author_data,
+        "column_data": column_data,
+        "font_data": font_data,
       }
       return template_file_data
     else: 
