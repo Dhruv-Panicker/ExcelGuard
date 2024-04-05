f5aa81edf36f33baa039b03678952f24433a6d7f
pJeyakumar <jn.piranavan@gmail.com>
2024-04-05 19:44:25 -0400

    feat: checking for similar fonts among files and including a count of how many similar fonts each file contains

--- algorithim/font_data.py
+++ algorithim/font_data.py
@@ -1,4 +1,40 @@
-def check_font_data(file_data):
-    # Your code to check the font data
-    # Return a score or a result indicating the likelihood of plagiarism
-    pass
+# A list of 25 commonly used fonts in excel
+COMMON_EXCEL_FONTS = {
+  "Calibri",
+  "Calibri Light",
+  "Arial",
+  "Times New Roman",
+  "Verdana",
+  "Tahoma",
+  "Cambria",
+  "Segoe UI",
+  "Helvetica",
+  "Garamond",
+  "Century Gothic",
+  "Comic Sans MS",
+  "Courier New",
+  "Georgia",
+  "Trebuchet MS",
+  "Lucida Sans Unicode",
+  "Franklin Gothic Medium",
+  "Arial Narrow",
+  "Impact",
+  "Book Antiqua",
+  "Consolas",
+  "Candara",
+  "Arial Black",
+  "MS Sans Serif",
+  "MS Serif",
+  "Palatino Linotype"
+}
+
+def check_font_data(font_data, template_data):
+  suspicious_fonts = {}
+  template_fonts = set(template_data["font_data"])
+  
+  for file, fonts in font_data.items():
+    similar_fonts = set(font for font in fonts if font not in COMMON_EXCEL_FONTS and font not in template_fonts)
+    font_count = len(similar_fonts)
+    suspicious_fonts[file] = (similar_fonts, font_count)
+
+  return suspicious_fonts
