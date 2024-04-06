cdd5dc1a52ce798e16eeb75f3f3910facac8290e
pJeyakumar <jn.piranavan@gmail.com>
2024-04-06 14:05:36 -0400

    feat: fixing check_font_data logic and creating pytest tests for check_font_data

--- algorithim/font_data.py
+++ algorithim/font_data.py
@@ -1,3 +1,4 @@
+from collections import Counter
 # A list of 25 commonly used fonts in excel
 COMMON_EXCEL_FONTS = {
   "Calibri",
@@ -29,20 +30,25 @@ COMMON_EXCEL_FONTS = {
 }
 
 def check_font_data(font_data, db, template_data, ExcelFile):
+  suspicious_fonts = {}
   template_fonts = set()
   if template_data:
     template_fonts = set(template_data["font_data"])
-  
+
+  uncommon_fonts = [font for fonts in font_data.values() for font in fonts if font not in template_fonts and font not in COMMON_EXCEL_FONTS]
+  font_counter = Counter(uncommon_fonts)
+  duplicate_uncommon_fonts = [font for font, count in font_counter.items() if count > 1]
+
   for file_id, fonts in font_data.items():
-    similar_fonts = set(font for font in fonts if font not in COMMON_EXCEL_FONTS and font not in template_fonts)
+    similar_fonts = [font for font in set(fonts) if font in duplicate_uncommon_fonts]
     font_count = len(similar_fonts)
+    suspicious_fonts[file_id] = (similar_fonts, font_count)
     try:
       excel_file = db.session.query(ExcelFile).filter_by(id=file_id).first()
       if excel_file:
-        excel_file.font_data_results = (list(similar_fonts), font_count)
+        excel_file.font_data_results = suspicious_fonts[file_id]
       db.session.commit()
     except Exception as e:
       db.session.rollback()
       print("Error updating excel file font data results attribute:", e)
-
-  return True
+  return suspicious_fonts
