59766583af22f600a0bd2cd661bebab66cadf40b
pJeyakumar <jn.piranavan@gmail.com>
2024-04-12 20:21:07 -0400

    chore: separating setup script into environment and environment setup scripts

---
+++ env.bat
@@ -0,0 +1,5 @@
+@echo off
+
+REM Create and activate virtual environment
+python -m venv venv
+venv\Scripts\activate
--- setup.bat
+++ setup.bat
@@ -1,13 +1,11 @@
 @echo off
 
-REM Create and activate virtual environment
-python -m venv venv
-venv\Scripts\activate
-
 REM Install dependencies
 python -m pip install -r requirements.txt
 
 REM Set environment variables within the virtual environment
-echo SET FLASK_APP=main > venv\Scripts\activate.bat
-echo SET FLASK_ENV=development >> venv\Scripts\activate.bat
-echo SET FLASK_DEBUG=1 >> venv\Scripts\activate.bat
+SET FLASK_APP=main
+SET FLASK_ENV=development
+SET FLASK_DEBUG=1
+
+flask run
