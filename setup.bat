f8131886f41004f114f8a34d7d2edd596a29383e
pJeyakumar <jn.piranavan@gmail.com>
2024-04-12 20:11:32 -0400

    chore: more setup work

--- setup.bat
+++ setup.bat
@@ -1,7 +1,13 @@
 @echo off
+
+REM Create and activate virtual environment
 python -m venv venv
 venv\Scripts\activate
+
+REM Install dependencies
 python -m pip install -r requirements.txt
-set FLASK_APP=main
-set FLASK_ENV=development
-set FLASK_DEBUG=1
+
+REM Set environment variables within the virtual environment
+echo SET FLASK_APP=main > venv\Scripts\activate.bat
+echo SET FLASK_ENV=development >> venv\Scripts\activate.bat
+echo SET FLASK_DEBUG=1 >> venv\Scripts\activate.bat
